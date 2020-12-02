7ae5056ae6e174acb7d8e07f22bc37ffad873fdf
SStupar2 <stefan.stupar2@gmail.com>
2020-12-02 13:44:39 +0100

    minor changes

--- frontend-rentacar/src/app/app.module.ts
+++ frontend-rentacar/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
+  //declaration of components that belongs to this module
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
+  //for services
   providers: [],
+  //starting component for our app
   bootstrap: [AppComponent]
 })
 export class AppModule { }
