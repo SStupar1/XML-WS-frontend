0c82436e4ccf2447063aefc446bbd98869c401df
nikolina424 <inina007@gmail.com>
2020-12-02 15:07:39 +0100

    Navbar, login and register components

--- frontend-rentacar/src/app/app-routing.module.ts
+++ frontend-rentacar/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegistrationComponent } from './pages/auth/registration/registration.component';
+import { NavbarComponent } from './pages/xml/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'navbar'},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-rentacar/src/app/app.module.ts
+++ frontend-rentacar/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { NavbarComponent } from './pages/xml/navbar/navbar.component';
+import { RegistrationComponent } from './pages/auth/registration/registration.component';
 
 @NgModule({
   //declaration of components that belongs to this module
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
