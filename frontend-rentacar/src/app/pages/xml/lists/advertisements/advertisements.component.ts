fa3ac3a5ddd57f0799bc91e89ea307a05db0a698
SStupar1 <sstefan98@gmail.com>
2021-01-08 20:28:00 +0100

    Create Ad Component

--- frontend-rentacar/src/app/pages/xml/lists/advertisements/advertisements.component.ts
+++ frontend-rentacar/src/app/pages/xml/lists/advertisements/advertisements.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdService } from 'src/app/services/ad.service';
-
+import { AgentService } from 'src/app/services/agent.service';
+import { SimpleUserService } from 'src/app/services/simple-user.service';
 
 @Component({
   selector: 'app-advertisements',
@@ -11,10 +12,15 @@ export class AdvertisementsComponent implements OnInit {
 
   public ads = [];
   private user: any;
+  checked = true;
+  private show: boolean;
+  private publisher : any;
+  private buttonName : any = 'Show publisher details';
 
-  constructor(private adService: AdService,) { }
+  constructor(private adService: AdService, private agentService: AgentService, private suService: SimpleUserService) {}
 
   ngOnInit(): void {
+    this.show = false;
     this.setupUser();
     this.getAllPublisherAds();
   }
@@ -30,5 +36,41 @@ export class AdvertisementsComponent implements OnInit {
       alert("Error");
     })
   }
+
+  private toggle(simpleUser, publisherId): void{
+    
+    if(!this.show){
+      this.buttonName = 'Hide';
+      this.show = true;
+    }else {
+      this.buttonName = 'Show publisher details';
+      this.show = false;
+    }
+
+    if(simpleUser){
+      this.suService.getSimpleUser(publisherId).subscribe(data =>{
+        this.publisher = data;
+      })
+    }else{
+      this.agentService.getAgent(publisherId).subscribe(data =>{
+        this.publisher = data;
+      })
+    }
+  }
+  /*
+
+  private getPublisher(simpleUser, publisherId): void {
+    if(simpleUser){
+      this.suService.getSimpleUser(publisherId).subscribe(data =>{
+        return data;
+      })
+    }else{
+      this.agentService.getAgent(publisherId).subscribe(data =>{
+        return data;
+      })
+    }
+  }
+*/
+
 
 }
--- frontend-rentacar/src/app/pages/xml/new-items/add-ad/add-ad.component.ts
+++ frontend-rentacar/src/app/pages/xml/new-items/add-ad/add-ad.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup ,Validators} from '@angular/forms';
+import { CarModelService } from 'src/app/services/car-model.service';
+import { FuelTypeService } from 'src/app/services/fuel-type.service';
+import { GearshiftTypeService } from 'src/app/services/gearshift-type.service';
+import {formatDate} from '@angular/common';
+import { AdService } from 'src/app/services/ad.service';
+
+
 
 @Component({
   selector: 'app-add-ad',
@@ -7,9 +15,112 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAdComponent implements OnInit {
 
-  constructor() { }
+  private user: any;
+  public carModels = [];
+  public carModelId : any;
+  public fuelTypes = [];
+  public fuelTypeId : any;
+  public gearshiftTypes = [];
+  public gearshiftTypeId : any;
+  private limitedDistance : boolean = true; 
+  validateForm!: FormGroup;
+  //CDW stavljam na false kada se kreira oglas(njega bira korisnik i u zavisnosti od toga se menja cena)
+  private cdw : boolean = false;
+  private simpleUser: boolean =false;
+
+
+
+
+
+  constructor(private adService: AdService,private fb: FormBuilder, private cmService: CarModelService, private ftService: FuelTypeService, private gtService: GearshiftTypeService) { }
 
   ngOnInit(): void {
+    this.setupUser();
+    this.getCarModels();
+    this.getFuelTypes();
+    this.getGearshiftTypes();
+    this.validateForm = this.fb.group({
+      kmTraveled: [null, [Validators.required]],
+      limitedKm: [null, [Validators.required]],
+      seats: [null, [Validators.required]]
+    });
+  }
+ 
+  private setupUser(): void {
+    this.user = JSON.parse(localStorage.getItem('user'));
+  } 
+
+  private getCarModels(): void {
+    this.cmService.getAllCarModels().subscribe(data => {
+      this.carModels = data;
+    }, error => {
+      alert("Error");
+    })
+  }
+  private getFuelTypes(): void {
+    this.ftService.getAllFuelTypes().subscribe(data => {
+      this.fuelTypes = data;
+    }, error => {
+      alert("Error");
+    })
+  }
+  private getGearshiftTypes(): void {
+    this.gtService.getAllGearshiftTypes().subscribe(data => {
+      this.gearshiftTypes = data;
+    }, error => {
+      alert("Error");
+    })
   }
 
+  changeSelectFuelType(e){
+    this.fuelTypeId = e.target.value;
+  }
+
+  changeSelectCarModel(e){
+    this.carModelId = e.target.value;
+  }
+
+  changeSelectGearshiftType(e){
+    this.gearshiftTypeId = e.target.value;
+  }
+
+  submitForm(): void {
+    //VALIDATE
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+
+    if(this.validateForm.value.kmTraveled == 0){
+      this.limitedDistance = false;
+    }
+
+    if(this.user.userRole == 'SIMPLE_USER'){
+      this.simpleUser = true;
+    }else{
+      this.simpleUser = false;
+    }
+    
+    const body = {
+      publisherId: this.user.id,
+      carModelId: this.carModelId,
+      fuelTypeId: this.fuelTypeId,
+      gearshiftTypeId: this.gearshiftTypeId,
+      kmTraveled: this.validateForm.value.kmTraveled,
+      limitedDistance: this.limitedDistance,
+      limitedKm: this.validateForm.value.limitedKm,
+      cdw: this.cdw,
+      seats: this.validateForm.value.seats,
+      creationDate: formatDate(new Date(), 'yyyy-MM-dd', 'en'),
+      simpleUser: this.simpleUser
+    }
+    console.log(body);
+
+    this.adService.createAd(body).subscribe(data => {
+      alert("Created! ");
+    }, error => {
+      alert("Error");
+    })
+
+  }
 }
